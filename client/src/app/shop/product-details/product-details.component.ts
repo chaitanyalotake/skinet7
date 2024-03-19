1f1869f519bf41829219d34ccf0d838692c71a87
Chaitanya <lotakec@gmail.com>
2024-03-19 09:02:45 +0530

    End of section 12

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,9 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
 
   product?: Product;
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) { 
+    this.bcService.set('@productDetails',' ');
+  }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
@@ -22,7 +25,10 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id)
       this.shopService.getProduct(+id).subscribe({
-        next: product => this.product = product,
+        next: product => {
+          this.product = product;
+          this.bcService.set('@productDetails',product.name)
+        },
         error: err => console.log(`Error: ${err}`)
       });
   }
